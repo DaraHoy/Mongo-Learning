b59558638005103f6bbc15a02b0184c829576269
Dara Hoy <darahoy@gmail.com>
2018-01-29 20:37:47 +0000

    Complete mongo level 1 requirements

--- test.js
+++ test.js
@@ -1,1 +1,43 @@
-console.log(process.env.PORT);
+/*global db*/
+console.log(new Date());
+db.pokemons.insert([{
+    "name": "Bulbasaur",
+    "health": 45,
+    "fought": "2018-01-29T17:33:15.430Z",
+    "attacks": ["tackle", "growl", "leech seed"],
+    "stats": {
+        "attack": 49,
+        "defense": 49
+    }
+},
+{
+    "name": "Charmander",
+    "health": 39,
+    "fought": "2018-01-29T17:33:15.430Z",
+    "attacks": ["growl", "scratch", "ember"],
+    "stats": {
+        "attack": 52,
+        "defense": 43
+    }
+},
+{
+    "name": "Squirtle",
+    "health": 44,
+    "fought": "2018-01-29T17:33:15.430Z",
+    "attacks": ["tackle", "tail whip", "water gun"],
+    "stats": {
+        "attack": 48,
+        "defense": 65
+    }
+}])
+
+function createPokemon (name, health, lastFought, attacks, stats, db){
+    var newPokemon = {}
+    newPokemon.name = name;
+    newPokemon.health = health;
+    newPokemon.lastFought = lastFought;
+    newPokemon.attacks = [];
+    newPokemon.stats = {};
+    newPokemon.db = db;
+    return newPokemon;
+};
